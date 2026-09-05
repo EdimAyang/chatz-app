0d3321672d4f91f471216141d51154036750d6e0
EdimAyang Emmanuel <jacksonprince590@gmail.com>
2026-09-05 01:44:08 +0100

    fix double push notfy

--- public/messaging-sw.js
+++ public/messaging-sw.js
@@ -6,27 +6,28 @@ importScripts(
   "https://www.gstatic.com/firebasejs/12.0.0/firebase-messaging-compat.js",
 );
 
+
+
 firebase.initializeApp({
-  apiKey: "AIzaSyBXyg2kLAfyWezheM7b27OzjRvZiYzrcRg",
+  apiKey:"AIzaSyBXyg2kLAfyWezheM7b27OzjRvZiYzrcRg",
   authDomain: "chatz-29664.firebaseapp.com",
   projectId: "chatz-29664",
   storageBucket: "chatz-29664.firebasestorage.app",
   messagingSenderId: "62212306014",
-  appId: "1:62212306014:web:470662fc9d0dc24b2cf0cf",
+  appId: "1:62212306014:web:470662fc9d0dc24b2cf0cf"
 });
+
+
 
 const messaging = firebase.messaging();
 
+
 messaging.onBackgroundMessage((payload) => {
-  const notification = payload.notification;
+  const notification = payload.webPush?.notification;
 
   if (!notification) return;
 
-  console.log(
-    "Displaying notification:",
-    notification.title,
-    notification.body,
-  );
+  console.log("Displaying notification:", notification.title, notification.body);
 
   self.registration.showNotification(notification.title, {
     body: notification.body,
@@ -35,31 +36,30 @@ messaging.onBackgroundMessage((payload) => {
     data: payload.data,
   });
 
+
   self.addEventListener("notificationclick", (event) => {
-    event.notification.close();
+  event.notification.close();
 
-    const data = event.notification.data;
+  const data = event.notification.data;
 
-    const url = data?.url || "/";
+  const url = data?.url || "/";
 
-    event.waitUntil(
-      clients
-        .matchAll({
-          type: "window",
-          includeUncontrolled: true,
-        })
-        .then((clientList) => {
-          // If Chatz is already open, focus it and navigate
-          for (const client of clientList) {
-            if ("focus" in client) {
-              client.navigate(url);
-              return client.focus();
-            }
-          }
+  event.waitUntil(
+    clients.matchAll({
+      type: "window",
+      includeUncontrolled: true,
+    }).then((clientList) => {
+      // If Chatz is already open, focus it and navigate
+      for (const client of clientList) {
+        if ("focus" in client) {
+          client.navigate(url);
+          return client.focus();
+        }
+      }
 
-          // Otherwise open Chatz
-          return clients.openWindow(url);
-        }),
-    );
-  });
+      // Otherwise open Chatz
+      return clients.openWindow(url);
+    })
+  );
+});
 });
