48fd16c113a764f34a40aaa855047d64eb562a31
EdimAyang Emmanuel <jacksonprince590@gmail.com>
2026-09-06 09:05:37 +0100

    fix notification icons and pop up style

--- public/messaging-sw.js
+++ public/messaging-sw.js
@@ -6,60 +6,55 @@ importScripts(
   "https://www.gstatic.com/firebasejs/12.0.0/firebase-messaging-compat.js",
 );
 
-
-
 firebase.initializeApp({
-  apiKey:"AIzaSyBXyg2kLAfyWezheM7b27OzjRvZiYzrcRg",
+  apiKey: "AIzaSyBXyg2kLAfyWezheM7b27OzjRvZiYzrcRg",
   authDomain: "chatz-29664.firebaseapp.com",
   projectId: "chatz-29664",
   storageBucket: "chatz-29664.firebasestorage.app",
   messagingSenderId: "62212306014",
-  appId: "1:62212306014:web:470662fc9d0dc24b2cf0cf"
+  appId: "1:62212306014:web:470662fc9d0dc24b2cf0cf",
 });
-
-
 
 const messaging = firebase.messaging();
 
-
 messaging.onBackgroundMessage((payload) => {
-  const notification = payload.webPush?.notification;
-
-  if (!notification) return;
-
-  console.log("Displaying notification:", notification.title, notification.body);
-
   self.registration.showNotification(notification.title, {
     body: notification.body,
-    icon: notification.icon || "/favicon.svg",
-    badge: notification.badge || "/favicon-16x16.png",
+
+    icon: notification.icon || "/icons/icon-192.png",
+    badge: notification.badge || "/icons/notification.png",
+    vibrate: [200, 100, 200],
+
+    requireInteraction: true,
+
     data: payload.data,
   });
 
-
   self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
+    event.notification.close();
 
-  const data = event.notification.data;
+    const data = event.notification.data;
 
-  const url = data?.url || "/";
+    const url = data?.url || "/";
 
-  event.waitUntil(
-    clients.matchAll({
-      type: "window",
-      includeUncontrolled: true,
-    }).then((clientList) => {
-      // If Chatz is already open, focus it and navigate
-      for (const client of clientList) {
-        if ("focus" in client) {
-          client.navigate(url);
-          return client.focus();
-        }
-      }
+    event.waitUntil(
+      clients
+        .matchAll({
+          type: "window",
+          includeUncontrolled: true,
+        })
+        .then((clientList) => {
+          // If Chatz is already open, focus it and navigate
+          for (const client of clientList) {
+            if ("focus" in client) {
+              client.navigate(url);
+              return client.focus();
+            }
+          }
 
-      // Otherwise open Chatz
-      return clients.openWindow(url);
-    })
-  );
-});
+          // Otherwise open Chatz
+          return clients.openWindow(url);
+        }),
+    );
+  });
 });
